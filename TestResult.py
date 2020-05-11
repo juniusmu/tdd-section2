d243c80eefbaba6f9be8594ae70c989cfdbeff81
Junius Murphy <juniusmu@umich.edu>
2020-05-10 21:26:38 -0400

    finish chapter 22

--- TestResult.py
+++ TestResult.py
@@ -1,9 +1,13 @@
 class TestResult:
     def __init__(self):
         self.runCount = 0
+        self.failCount = 0
 
     def testStarted(self):
         self.runCount = self.runCount + 1
 
+    def testFailed(self):
+        self.failCount = self.failCount + 1
+
     def summary(self):
-        return "%d run, 0 failed" % self.runCount
+        return "%d run, 0 failed" % (self.runCount, self.errorCount)
